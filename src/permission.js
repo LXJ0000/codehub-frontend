dc24867e62870502ca92b9a32686572496eb64d4
Naru11 <2925315190@qq.com>
2024-11-17 22:03:47 +0800

    feat: refreshToken

--- src/permission.js
+++ src/permission.js
@@ -1,21 +1,20 @@
 import router from '@/router'
-// import { usePermissionStore } from './store/modules/permission'
 import { useUserStore } from './store/modules/user'
+import { isTokenExpired } from '@/utils/checkToken'
 
 const whiteList = ['/login', '/404', '/register']
 router.beforeEach(async (to, from, next) => {
-  // console.log(usePermissionStore)
   const accessToken = useUserStore().accessToken
-  // const permissionStore = usePermissionStore
+  const refreshToken = useUserStore().refreshToken
+  const refreshTokenFunc = useUserStore().refreshTokenFunc
+
+  if (accessToken && (isTokenExpired(accessToken))) {
+    await refreshTokenFunc({ refresh_token: refreshToken })
+  }
   if (accessToken) {
     if (to.path === '/login') {
       next({ path: '/' })
     } else {
-      // if (!permissionStore.permissionRoutes?.length) {
-      //   const routes = await permissionStore.getPermissionRoute()
-      //   routes.forEach((item) => router.addRoute(item))
-      //   return next(to.path)
-      // }
       next()
     }
   } else {
